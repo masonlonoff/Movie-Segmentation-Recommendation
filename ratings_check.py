ea756f98620ad4d6a3f9c4360aa4dced4581daaf
masonlonoff <117112918+masonlonoff@users.noreply.github.com>
2025-05-07 22:57:14 -0400

    Update ratings_check.py
    adding clean codes

--- ratings_check.py
+++ ratings_check.py
@@ -3,11 +3,11 @@ from pyspark.sql import SparkSession
 def main():
     spark = SparkSession.builder.appName("Inspect Ratings Full").getOrCreate()
     
-    # Adjust this path if needed
+
     path = "hdfs:///user/ml9542_nyu_edu/ratings_full.parquet"
     df = spark.read.parquet(path)
 
-    # Show schema and a few rows
+
     df.printSchema()
     df.show(10, truncate=False)
 
